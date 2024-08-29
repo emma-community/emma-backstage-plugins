a52468ecdda4ddb6ee902dfb19902dbb7452bee7
Tobias Andersen <tobias.andersen79@gmail.com>
2024-08-29 15:36:51 +0200

    expose getDataCenters route

--- plugins/emma-backend/src/service/router.ts
+++ plugins/emma-backend/src/service/router.ts
@@ -2,16 +2,20 @@ import { MiddlewareFactory } from '@backstage/backend-defaults/rootHttpRouter';
 import { LoggerService, RootConfigService } from '@backstage/backend-plugin-api';
 import express from 'express';
 import Router from 'express-promise-router';
+import { EmmaApiImpl } from '../api';
+import { EmmaDataCenter } from '@internal/backstage-plugin-emma-common';
 
 export interface RouterOptions {
   logger: LoggerService;
   config: RootConfigService;
+  emmaApi?: EmmaApiImpl;
 }
 
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
   const { logger, config } = options;
+  const emmaApi = options.emmaApi ?? new EmmaApiImpl();
 
   const router = Router();
   router.use(express.json());
@@ -21,8 +25,29 @@ export async function createRouter(
     response.json({ status: 'ok' });
   });
 
+  router.get('/datacenters', async (req, res) => {
+    const latMax = req.query.latMax ? Number(req.query.latMax) : undefined;
+    const lonMax = req.query.lonMax ? Number(req.query.lonMax) : undefined;
+    const latMin = req.query.latMin ? Number(req.query.latMin) : undefined;
+    const lonMin = req.query.lonMin ? Number(req.query.lonMin) : undefined;
+
+    let dataCenters: EmmaDataCenter[];
+
+    if(latMax !== undefined && lonMax !== undefined && latMin !== undefined && lonMin !== undefined) {
+      dataCenters = await emmaApi.getDataCenters({ topRight: { latitude: Number(req.query.latMax), longitude: Number(req.query.lonMax) }, bottomLeft: { latitude: Number(req.query.latMin), longitude: Number(req.query.lonMin) } });
+    }
+    else {
+      dataCenters = await emmaApi.getDataCenters();
+    }
+
+    res.status(200).json(dataCenters);
+  });
+
   const middleware = MiddlewareFactory.create({ logger, config });
 
   router.use(middleware.error());
+
+  console.log(router);
+
   return router;
 }
