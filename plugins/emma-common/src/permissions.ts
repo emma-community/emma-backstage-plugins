d1dcfd818bc9e25c7401ea9e0899dfb0903bff60
Tobias Andersen <tobias.andersen79@gmail.com>
2024-09-04 01:23:07 +0200

    update permission TODO with ref to new permission system in Backstage

--- plugins/emma-common/src/permissions.ts
+++ plugins/emma-common/src/permissions.ts
@@ -2,14 +2,13 @@ import { createPermission } from '@backstage/plugin-permission-common';
 import { RESOURCE_TYPE_CATALOG_ENTITY } from '@backstage/plugin-catalog-common/alpha';
 import { EMMA_PLUGIN_ID } from './constants';
 
-// TODO: Implement permissions in backend plugin
+// TODO: Migrate this to new permission architecture @ https://backstage.io/docs/permissions/getting-started/
 /** @public */
 export const emmaDataCenterReadPermission = createPermission({
   name: EMMA_PLUGIN_ID + '.api.datacenter.read',
   attributes: { action: 'read' },
   resourceType: RESOURCE_TYPE_CATALOG_ENTITY,
 });
-
 
 /** @public */
 export const emmaPermissions = [
